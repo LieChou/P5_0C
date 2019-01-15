5edf0209abbb7524bb1265a5cfa12f6aa417b00a
AurelieT <aurelie.tudare@gmail.com>
2019-01-15 16:23:43 +0100

    mise à jour de la page d'accueil pour l'étape 1 du projet

--- js/main.js
+++ js/main.js
@@ -1,64 +1,64 @@
-var citation = document.querySelector('.citation');
-var random = document.querySelector('.random');
+let citation = document.querySelector('.citation');
+let random = document.querySelector('.random');
 
-//function randomValueFromArray(array){return array[Math.floor(Math.random()*array.length)];}
-
-var quotes =["l\'amour c\'est l'\idéal de l\'égalité", 
-"je voudrais être Maria", 
-"il y a La Callas qui exige que je me porte avec sa dignité", 
-"une différence de goût", 
-"en matière de plaisanteries gâte l'amitié", 
-"la connaissance qu'a un seul homme de la faute de cent autres", 
-"ne lui sert à rien", 
-"quand on aime la vie on aime le passé", 
-"c'est le présent tel qu'il a survécu dans la mémoire humaine", 
-"ce dont nous avons besoin est d'aimer", 
-"sans nous épuiser"];
-
-console.log(quotes.length);
-
-//fonction anonyme 
 random.addEventListener('click', ()=> {
+
+    var quotes = ["l\'amour c\'est l'\idéal de l\'égalité", 
+    "je voudrais être Maria", 
+    "il y a La Callas qui exige que je me porte avec sa dignité", 
+    "une différence de goût", 
+    "en matière de plaisanteries gâte l'amitié", 
+    "la connaissance qu'a un seul homme de la faute de cent autres", 
+    "ne lui sert à rien", 
+    "quand on aime la vie on aime le passé", 
+    "c'est le présent tel qu'il a survécu dans la mémoire humaine", 
+    "ce dont nous avons besoin est d'aimer", 
+    "sans nous épuiser",
+    "le refuge le plus sur",
+    "est le coeur d'une maman",
+    "tout vient à point",
+    "à qui sait attendre",
+    "la vie c'est comme une bicyclette", 
+    "il faut avancer pour ne pas perdre l'équilibre", 
+    "un seul être vous manque", 
+    "et tout est dépeuplé", 
+    "dans la vie on ne fait pas ce que l'on veut", 
+    "on est responsable de ce que l'on est", 
+    "le bonheur est parfois caché", 
+    "dans l'inconnu", 
+    "le monde ne sera pas détruit par ceux qui font le mal", 
+    "par ceux qui les regardent sans rien faire",
+    "il ne faut pas",
+    "vendre la peau de l'ours",
+    "avant de l'avoir tué",
+    "le monde du passe-temps",
+    "de la montre qui tourne"]
+
+    let viewedQuotes=[];
+
     function getRandomQuote(){
         //splice the object from the quotes array and store it into a new variable "splicedQuote"
-        var randomQuote = Math.floor(Math.random()*quotes.length);
-        var splicedQuote = quotes.splice(randomQuote,1)[0];
-    
-        //push the spliced object into the "viewedQuotes" array
-        viewedQuotes=[];
-        viewedQuotes.push(splicedQuote);
-    
-        //"if" statement to check if first array is now empty = all quotes have been used
-        // if true then change it back to original state and set viewQuotes to an empty array
-        if (quotes.length == 0){
+        let randomQuote = Math.floor(Math.random()*quotes.length);
+        let splicedQuote = quotes.splice(randomQuote,1)[0];
+        viewedQuotes.push(splicedQuote); 
+        if (quotes.length === 0){
             quotes = viewedQuotes;
             viewedQuotes=[];
         }
         return splicedQuote;
         }
         
-    
-    function generate(quotes){
-        for (i=0; i<3; i++){
-            generate += quotes.selectedRandomQuote[i];
-        }
-        return generate;
-    }
+        function generateQuote(quotes){
+            let part1 = getRandomQuote(quotes);
+            let part2 = getRandomQuote(quotes);
+            let part3 = getRandomQuote(quotes);
+            let newCitation = part1.charAt(0).toUpperCase() + part1.slice(1) + ": " + part2 + ", " + part3 + "."; 
+            citation.textContent = newCitation;
+            citation.style.visibility = 'visible';
+            console.log(newCitation);
+            }  
 
-    var selectedRandomQuote = getRandomQuote;
-    citation.textContent = selectedRandomQuote;
-    citation.style.visibility = 'visible';
-    console.log(selectedRandomQuote);
+        generateQuote(quotes);
+        
 });
     
-    //faire une boucle for et appeler tableau
-     
-    
-   //if ((part1 !== part2 ) && (part2 !==part3) && (part3 !== part1)){
-    //var newCitation = part1.charAt(0).toUpperCase() + part1.slice(1) + ": " + part2 + ", " + part3 + ".";
-    //console.log(newCitation);
-   //}
-   //else {
-    //result();
-   //}
-
